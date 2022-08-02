ad06f79ac94ee05365d9de4866a6349b449dfe87
RawrRoss <randyross2000@gmail.com>
2022-08-02 10:43:11 -0700

    Update to 1.19

--- src/main/java/com/github/ndrp/fasterminecarts/FasterMinecarts.java
+++ src/main/java/com/github/ndrp/fasterminecarts/FasterMinecarts.java
@@ -1,14 +1,15 @@
 package com.github.ndrp.fasterminecarts;
 
 import com.github.ndrp.fasterminecarts.config.FasterMinecartsConfig;
-
-import me.sargunvohra.mcmods.autoconfig1u.AutoConfig;
-import me.sargunvohra.mcmods.autoconfig1u.serializer.GsonConfigSerializer;
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ModInitializer;
 
 public class FasterMinecarts implements ModInitializer {
-	@Override
-	public void onInitialize() {
-		AutoConfig.register(FasterMinecartsConfig.class, GsonConfigSerializer::new);
-	}
+
+    @Override
+    public void onInitialize() {
+        AutoConfig.register(FasterMinecartsConfig.class, GsonConfigSerializer::new);
+    }
+
 }
--- src/main/java/com/github/ndrp/fasterminecarts/config/FasterMinecartsConfig.java
+++ src/main/java/com/github/ndrp/fasterminecarts/config/FasterMinecartsConfig.java
@@ -1,21 +1,21 @@
 package com.github.ndrp.fasterminecarts.config;
 
-import me.sargunvohra.mcmods.autoconfig1u.ConfigData;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.Config;
-import me.sargunvohra.mcmods.autoconfig1u.annotation.ConfigEntry;
+import me.shedaniel.autoconfig.ConfigData;
+import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "faster-minecarts")
 public class FasterMinecartsConfig implements ConfigData {
-	
-	@ConfigEntry.Gui.Excluded
-	public double slowSpeed = 0.3;
-	
-	@ConfigEntry.Gui.Tooltip(count = 2)
-	@ConfigEntry.Category("default")
+
+    @ConfigEntry.Gui.Excluded
+    public double slowSpeed = 0.3;
+
+    @ConfigEntry.Gui.Tooltip(count = 2)
+    @ConfigEntry.Category("default")
     public boolean automaticMinecartSlowDown = true;
-	
-	@ConfigEntry.Gui.Tooltip()
-	@ConfigEntry.BoundedDiscrete(max = 12)
-	@ConfigEntry.Category("default")
+
+    @ConfigEntry.Gui.Tooltip()
+    @ConfigEntry.BoundedDiscrete(max = 12)
+    @ConfigEntry.Category("default")
     public int maxSpeed = 8;
 }
